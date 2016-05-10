f0fe7d19a9857e4cdb84c6a3597272496c983396
Christhian Gomez <chris093.013@gmail.com>
2016-05-10 16:41:27 -0500

    Was added an signal out, ICC

--- FinalProcessor/PSR.vhd
+++ FinalProcessor/PSR.vhd
@@ -7,6 +7,7 @@ entity PSR is
 			  nCWP : in STD_LOGIC;
            Operand2 : in  STD_LOGIC_VECTOR (31 downto 0);
            NZVC : in  STD_LOGIC_VECTOR (3 downto 0);
+			  ICC : out STD_LOGIC_VECTOR (3 downto 0);
            Carry : out  STD_LOGIC;
 			  CWP : out STD_LOGIC);
 end PSR;
@@ -24,12 +25,14 @@ begin
 		if(Reset = '1') then
 			Carry <= '0';
 			CWP <= '0'; -- Windows 0
+			ICC <= "0000";
 		else
 			if(rising_edge(CLK)) then
 				PSRRegister(0) <= nCWP; -- [cwp]
 				PSRRegister(23 downto 20) <= NZVC; -- [ICC]
 				Carry <= PSRRegister(20); -- C bit
 				CWP <= PSRRegister(0); -- cwp 1 bit
+				ICC <= PSRRegister(23 downto 20); -- [ICC]
 			end if;
 		end if;
 	end process;
