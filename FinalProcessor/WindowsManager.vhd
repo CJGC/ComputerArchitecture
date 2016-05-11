330e4e3cb960eafc6b1fe5d2eade90c981360ed4
Christhian Gomez <chris093.013@gmail.com>
2016-05-11 15:04:38 -0500

    There were some optimizations on the code

--- FinalProcessor/WindowsManager.vhd
+++ FinalProcessor/WindowsManager.vhd
@@ -21,34 +21,28 @@ architecture Behavioral of WindowsManager is
 
 	impure function setR(Rn : STD_LOGIC_VECTOR; CWP : STD_LOGIC) return STD_LOGIC_VECTOR is
 	variable newRegister : std_logic_vector (5 downto 0);
-	begin--  		In registers 24 - 31
-		if(Rn >= "11000" and Rn <= "11111")then
-			newRegister := conv_std_logic_vector(conv_integer(Rn) - (conv_integer(CWP) * 16),6);
-		else-- 				Local register 16 - 23 and Out registers 8 - 15
-			if((Rn >= "10000" and Rn <= "10111") or (Rn >= "01000" and Rn <= "01111"))then
+	begin
+		if(Rn >= "11000" and Rn <= "11111")then --In registers 24 - 31
+			newRegister := conv_std_logic_vector(conv_integer(Rn) - (conv_integer(CWP) * 16),6);						
+		elsif((Rn >= "10000" and Rn <= "10111") or (Rn >= "01000" and Rn <= "01111"))then -- Local register 16 - 23 and Out registers 8 - 15
 				newRegister := conv_std_logic_vector(conv_integer(Rn) + (conv_integer(CWP) * 16),6);
-			else -- 		Global register 0 - 7
-				if(Rn >= "00000" and Rn <= "00111")then
+		elsif(Rn >= "00000" and Rn <= "00111")then --Global register 0 - 7
 					newRegister := '0'&Rn; -- Concatenate 0 with Rn
- 				end if;
-			end if;
 		end if;
 		return newRegister;
 	end function;
 
 begin
 	process(OP,OP3,RS1,RS2,RD,CWP)
-	begin --							  SAVE
-		if(OP = "10" and (OP3 = "111100"))then
-			nCWP <= '0'; -- new current windows pointer
-		else --							 RESTORE
-			if(OP = "10" and (OP3 = "111101"))then
-				nCWP <= '1'; -- new current windows pointer
-			end if;
+	begin
+		if(OP = "10" and (OP3 = "111100"))then-- SAVE
+			nCWP <= '0'; -- New current windows pointer
+		elsif(OP = "10" and (OP3 = "111101"))then -- RESTORE
+			nCWP <= '1'; -- New current windows pointer
 		end if;
-		nRS1 <= setR(RS1,CWP); -- new register source 1
-		nRS2 <= setR(RS2,CWP); -- new register source 2
-		nRD <= setR(RD,CWP); -- new register destiny
-		Register07 <= conv_std_logic_vector(conv_integer(CWP)*16 + 15,6); -- 07 out
+		nRS1 <= setR(RS1,CWP); -- New register source 1
+		nRS2 <= setR(RS2,CWP); -- New register source 2
+		nRD <= setR(RD,CWP); -- New register destination
+		Register07 <= conv_std_logic_vector(conv_integer(CWP)*16 + 15,6); -- Register 07
 	end process;
 end Behavioral;
