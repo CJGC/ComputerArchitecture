78cc4a1d05e3c72777bc0efd9621b392247c5e12
Christhian Gomez <chris093.013@gmail.com>
2016-05-10 20:49:13 -0500

    Test bench belonging modified PSR module to adapt to new changes

--- FinalProcessor/PSR_tb.vhd
+++ FinalProcessor/PSR_tb.vhd
@@ -12,20 +12,26 @@ ARCHITECTURE behavior OF PSR_tb IS
     PORT(
          CLK : IN  std_logic;
          Reset : IN  std_logic;
+			nCWP : IN STD_LOGIC;
          Operand2 : IN  std_logic_vector(31 downto 0);
          NZVC : IN  std_logic_vector(3 downto 0);
-         Carry : OUT  std_logic
+         Carry : OUT  std_logic;
+			ICC : OUT STD_LOGIC_VECTOR (3 downto 0);
+			CWP : OUT STD_LOGIC
         );
     END COMPONENT;
 
    --Inputs
    signal CLK : std_logic := '1';
    signal Reset : std_logic := '0';
+	signal nCWP : std_logic := '0'; -- Window 0
    signal Operand2 : std_logic_vector(31 downto 0) := (others => '0');
    signal NZVC : std_logic_vector(3 downto 0) := (others => '0');
 
  	--Outputs
    signal Carry : std_logic;
+	signal ICC : STD_LOGIC_VECTOR (3 downto 0);
+	signal CWP : STD_LOGIC;
 
    -- Clock period definitions
    constant CLK_period : time := 10 ns;
@@ -36,9 +42,12 @@ BEGIN
    uut: PSR PORT MAP (
           CLK => CLK,
           Reset => Reset,
+			 nCWP => nCWP,
           Operand2 => Operand2,
           NZVC => NZVC,
-          Carry => Carry
+          Carry => Carry,
+			 ICC => ICC,
+			 CWP => CWP
         );
 
    -- Clock process definitions
@@ -55,6 +64,7 @@ BEGIN
    begin		
       wait for 100 ns;
 		NZVC <= "0001";
+		nCWP <= '1';
 		wait for 100 ns;
 		Reset <= '1';
       wait;
