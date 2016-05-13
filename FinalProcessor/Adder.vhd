bf220e7ceef938a78ff082745ecfdfea5676d9f8
Christhian Gomez <chris093.013@gmail.com>
2016-05-13 18:46:05 -0500

    Adder was modified AdderInput1, nPCout_PCin, AdderOut to 32 bits signals

--- FinalProcessor/Adder.vhd
+++ FinalProcessor/Adder.vhd
@@ -3,9 +3,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 
 entity Adder is
-    Port ( AdderInput1 : in  STD_LOGIC_VECTOR (5 downto 0);
-           nPCout_PCin : in  STD_LOGIC_VECTOR (5 downto 0);
-           AdderOut : out  STD_LOGIC_VECTOR (5 downto 0));
+    Port ( AdderInput1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           nPCout_PCin : in  STD_LOGIC_VECTOR (31 downto 0);
+           AdderOut : out  STD_LOGIC_VECTOR (31 downto 0));
 end Adder;
 
 architecture Behavioral of Adder is
