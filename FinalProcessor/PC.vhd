0e29078a35abde26e08b89ef4c366195c34211b6
Christhian Gomez <chris093.013@gmail.com>
2016-05-13 18:27:22 -0500

    Program counter was modified PCin and address to 32 bits signals

--- FinalProcessor/PC.vhd
+++ FinalProcessor/PC.vhd
@@ -4,8 +4,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity PC is
     Port ( CLK : in  STD_LOGIC;
            rst : in  STD_LOGIC;
-           PCin : in  STD_LOGIC_VECTOR (5 downto 0);
-           address : out  STD_LOGIC_VECTOR (5 downto 0));
+           PCin : in  STD_LOGIC_VECTOR (31 downto 0);
+           address : out  STD_LOGIC_VECTOR (31 downto 0));
 end PC;
 
 architecture Behavioral of PC is
