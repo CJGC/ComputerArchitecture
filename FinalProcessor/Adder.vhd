a74d8e9dea0ba00c8eba8c3f91d39d5a6ef41013
Christhian Gomez <chris093.013@gmail.com>
2016-05-14 14:00:22 -0500

    Was changed Input names to Input1 and Input2

--- FinalProcessor/Adder.vhd
+++ FinalProcessor/Adder.vhd
@@ -3,14 +3,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 
 entity Adder is
-    Port ( AdderInput1 : in  STD_LOGIC_VECTOR (31 downto 0);
-           nPCout_PCin : in  STD_LOGIC_VECTOR (31 downto 0);
+    Port ( Input1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           Input2 : in  STD_LOGIC_VECTOR (31 downto 0);
            AdderOut : out  STD_LOGIC_VECTOR (31 downto 0));
 end Adder;
 
 architecture Behavioral of Adder is
 
 begin
-	AdderOut <= nPCout_PCin + AdderInput1;
+	AdderOut <= Input1 + Input2;
 end Behavioral;
 
