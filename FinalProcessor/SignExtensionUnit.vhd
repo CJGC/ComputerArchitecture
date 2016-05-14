dd21d85f694071f2efcdfb8afa73ac47032c89b4
Christhian Gomez <chris093.013@gmail.com>
2016-05-14 15:51:40 -0500

    Sign extension unit was modified completly and is finished and working properly

--- FinalProcessor/SignExtensionUnit.vhd
+++ FinalProcessor/SignExtensionUnit.vhd
@@ -3,24 +3,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SignExtensionUnit is
     Port ( IMM13 : in  STD_LOGIC_VECTOR (12 downto 0);
-           IMM13Extended : out  STD_LOGIC_VECTOR (31 downto 0));
+			  Disp22 : in STD_LOGIC_VECTOR (21 downto 0);
+			  Disp30 : in STD_LOGIC_VECTOR (29 downto 0);
+			  Selector : in STD_LOGIC_VECTOR (1 downto 0);
+           SignExtensionOut : out  STD_LOGIC_VECTOR (31 downto 0));
 end SignExtensionUnit;
 
 architecture Behavioral of SignExtensionUnit is
 
-	signal packOne : std_logic_vector (31 downto 0) := (others=>'1');
-	signal packZero : std_logic_vector (31 downto 0) := (others => '0');
-	
+	impure function SignExtend(incompletePack : std_logic_vector;packOne : std_logic_vector;packZero : std_logic_vector;lastBit : integer) return std_logic_vector is
+	variable completePack : std_logic_vector (31 downto 0);
+	begin
+		if(incompletePack(lastBit) = '1')then completePack := packOne&incompletePack; -- Negative number
+		else completePack := packZero&incompletePack; -- Positive number
+		end if;
+		return completePack;
+	end function;
+
 begin
 
-	process(IMM13,packOne,packZero)
+	process(Selector,IMM13,Disp22,Disp30)
 	begin
-		if(IMM13(12) = '1')then -- Negative number
-			packOne(12 downto 0) <= IMM13;
-			IMM13Extended <= packOne;
-		else							-- Positive number
-			packZero(12 downto 0) <= IMM13;
-			IMM13Extended <= packZero;
+		if(Selector = "00")then SignExtensionOut <= SignExtend(IMM13,"1111111111111111111","0000000000000000000",12); -- Extent IMM13
+		elsif(Selector = "01")then SignExtensionOut <= SignExtend(Disp22,"1111111111","0000000000",21); -- Extent Disp22
+		elsif(Selector = "10")then SignExtensionOut <= SignExtend(Disp30,"11","00",29); -- Extent Disp30
 		end if;
 	end process;
 
