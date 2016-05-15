0f6c748679d6e3a96f6f5157e6aeb801096991cd
Christhian Gomez <chris093.013@gmail.com>
2016-05-15 17:46:54 -0500

    Was deleted Operand2 signal, and I have performed little code tweaks

--- FinalProcessor/PSR.vhd
+++ FinalProcessor/PSR.vhd
@@ -5,7 +5,6 @@ entity PSR is
     Port ( CLK : in  STD_LOGIC;
            Reset : in  STD_LOGIC;
 			  nCWP : in STD_LOGIC;
-           Operand2 : in  STD_LOGIC_VECTOR (31 downto 0);
            NZVC : in  STD_LOGIC_VECTOR (3 downto 0);
 			  ICC : out STD_LOGIC_VECTOR (3 downto 0);
            Carry : out  STD_LOGIC;
@@ -13,26 +12,21 @@ entity PSR is
 end PSR;
 
 architecture Behavioral of PSR is
-
 	-- [31:28], [27:24], [23:20], [19:14],  [13], [12], [11:8], [7], [6], [5], [4:0]
 	-- [Impl],   [Ver],   [ICC], [Reserved],[EC], [EF], [PIL],  [S], [PS],[ET],[CWP]
-	signal PSRRegister : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
-
 begin
 
-	process(CLK,Reset,nCWP,Operand2,NZVC,PSRRegister)
+	process(CLK,Reset,nCWP,NZVC)
 	begin
 		if(Reset = '1') then
-			Carry <= '0';
-			CWP <= '0'; -- Windows 0
-			ICC <= "0000";
+			Carry <= '0'; -- Default value
+			CWP <= '0'; -- Windows 0 ; Default value
+			ICC <= "0000"; -- Default value
 		else
 			if(rising_edge(CLK)) then
-				PSRRegister(0) <= nCWP; -- [cwp]
-				PSRRegister(23 downto 20) <= NZVC; -- [ICC]
-				Carry <= PSRRegister(20); -- C bit
-				CWP <= PSRRegister(0); -- CWP bit
-				ICC <= PSRRegister(23 downto 20); -- NZVC
+				Carry <= NZVC(0); -- C bit
+				CWP <= nCWP; -- CWP bit
+				ICC <= NZVC; -- NZVC
 			end if;
 		end if;
 	end process;
