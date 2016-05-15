51085e4584384c69c464367a78e38558272f65c1
Christhian Gomez <chris093.013@gmail.com>
2016-05-15 12:37:20 -0500

    Test bench belonging to sign extension unit module was change for adapt to new changes

--- FinalProcessor/SignExtensionUnit_tb.vhd
+++ FinalProcessor/SignExtensionUnit_tb.vhd
@@ -20,9 +20,9 @@ ARCHITECTURE behavior OF SignExtensionUnit_tb IS
 
    --Inputs
    signal IMM13 : std_logic_vector(12 downto 0) := (others => '0');
-   signal Disp22 : std_logic_vector(21 downto 0) := (others => '0');
+   signal Disp22 : std_logic_vector(21 downto 0) := "0101010101010101010010";
    signal Disp30 : std_logic_vector(29 downto 0) := (others => '0');
-   signal Selector : std_logic_vector(1 downto 0) := (others => '0'); -- Extent IMM13
+   signal Selector : std_logic_vector(1 downto 0) := (others => '0'); -- Extent Disp22
 
  	--Outputs
    signal SignExtensionOut : std_logic_vector(31 downto 0);
@@ -42,10 +42,14 @@ BEGIN
    stim_proc: process
    begin
       wait for 100 ns;
-      Selector <= "01"; -- Extent Disp 22
-		wait for 100 ns;
-		Selector <= "10"; -- Extent Disp 30
 		Disp30 <= "101010101010101010101010101010";
+      Selector <= "01"; -- Extent Disp 30
+		wait for 100 ns;
+		IMM13 <= "0000000001010";
+		Selector <= "10"; -- Extent IMM13
+		wait for 100 ns;
+		IMM13 <= "0000000000000";
+		Selector <= "11"; -- Extent IMM13
 		wait;
    end process;
 
